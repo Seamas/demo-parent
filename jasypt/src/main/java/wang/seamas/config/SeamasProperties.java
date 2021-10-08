ffd2c30d28302a6c8ad45a06e21188c30aa493ed
Seamas.Wang <seamas.wang@gmail.com>
2021-10-08 09:38:14 +0800

    增加同时响应json和xml配置

--- jasypt/src/main/java/wang/seamas/config/SeamasProperties.java
+++ jasypt/src/main/java/wang/seamas/config/SeamasProperties.java
@@ -1,5 +1,7 @@
 package wang.seamas.config;
 
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
@@ -7,8 +9,11 @@ import org.springframework.stereotype.Component;
 @Data
 @Component
 @ConfigurationProperties(prefix = "seamas")
+@JacksonXmlRootElement(localName = "User")
 public class SeamasProperties {
 
+    @JacksonXmlProperty(localName = "name")
     private String username;
+    @JacksonXmlProperty(localName = "pwd")
     private String password;
 }
--- jasypt/src/main/java/wang/seamas/controller/HelloController.java
+++ jasypt/src/main/java/wang/seamas/controller/HelloController.java
@@ -2,6 +2,7 @@ package wang.seamas.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,7 +20,7 @@ public class HelloController {
     SeamasProperties properties;
 
 
-    @GetMapping("prop")
+    @GetMapping(value = "prop", produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
     public Object prop() {
         Object obj = properties;
         log.info("obj : {}", obj);
@@ -28,8 +29,6 @@ public class HelloController {
 
     @GetMapping("user")
     public Object user(@Valid User user) {
-
-
 
         return "hello";
     }
